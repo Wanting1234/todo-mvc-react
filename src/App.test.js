0753f009da10e09cf06233722eb0caec63ec96b5
Ting Wang <ting.wang@thoughtworks.com>
2022-11-16 22:06:05 +0800

    chore: add dependencies for test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import App from './App';
 
-test('renders learn react link', () => {
+test('render hello world', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const text = screen.getByText('Hello, world!');
+  expect(text).toBeInTheDocument();
 });
