f109cb6b44c5091c4a85405c37b0ed99d7499599
Ting Wang <ting.wang@thoughtworks.com>
2022-11-17 16:19:52 +0800

    feat: should not add empty input to task list

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
-function AddTask() {
-    return(
-        <input className="new-task" placeholder='What needs to be done?' autoFocus/>
-    );
-}
+const AddTask = () => {
+  const [taskName, setTaskName] = useState("");
+
+  const changeTaskName = (e) => {
+    setTaskName(e.target.value);
+  };
+
+  const handleKeyDown = (e) => {
+    const enterCode = 13;
+    const isEnter = e.keyCode === enterCode;
+    const newTaskName = taskName.trim();
+    const isTaskNamePresent = newTaskName.length > 0;
+    if (isEnter && isTaskNamePresent) {
+      e.target.value = "";
+    }
+  };
+
+  return (
+    <input
+      className="new-task"
+      placeholder="What needs to be done?"
+      autoFocus
+      value={taskName}
+      onChange={changeTaskName}
+      onKeyDown={handleKeyDown}
+    />
+  );
+};
 
 export default AddTask;
--- src/tests/AddTask.test.js
+++ src/tests/AddTask.test.js
@@ -1,12 +1,23 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen } from "@testing-library/react";
 import AddTask from "../components/AddTask";
+import userEvent from "@testing-library/user-event";
 
-describe('add task input box', () => {
-    test('should render input box', () => {
-        // arrange
-        // act
-        render(<AddTask />);
-        // assert
-        expect(screen.getByRole('textbox')).toBeInTheDocument();
-    });
-})
+describe("add task input box", () => {
+  test("should render input box", () => {
+    // arrange
+    // act
+    render(<AddTask />);
+    // assert
+    expect(screen.getByRole("textbox")).toBeInTheDocument();
+  });
+
+  test("should clear input value when enter key pressed", () => {
+    render(<AddTask />);
+
+    userEvent.type(screen.getByRole("textbox"), "test");
+    expect(screen.getByRole("textbox")).toHaveValue("test");
+
+    userEvent.keyboard("{enter}");
+    expect(screen.getByRole("textbox")).toHaveValue("");
+  });
+});
