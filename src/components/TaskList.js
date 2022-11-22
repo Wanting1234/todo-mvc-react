01a6b188a784da51599bc5583c21eba0ba1386b0
Ting Wang <ting.wang@thoughtworks.com>
2022-11-22 13:20:37 +0800

    refactor: extract function from main section to task list

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,5 +1,37 @@
 import React from "react";
+import { TaskItem } from "./TaskItem";
 
-export const TaskList = (props) => (
-  <ul className="task-list">{props.tasks.map(props.prop1)}</ul>
-);
+export const TaskList = ({
+  tasks,
+  filter,
+  updateStatus,
+  deleteTask,
+  editTask,
+}) => {
+  const getVisibleTasks = () => {
+    if (filter === "active") {
+      return tasks.filter((task) => !task.completed);
+    } else if (filter === "completed") {
+      return tasks.filter((task) => task.completed);
+    }
+    return tasks;
+  };
+
+  const visibleTasks = getVisibleTasks();
+
+  return (
+    <section className="task-list">
+      <ul>
+        {visibleTasks.map((task) => (
+          <TaskItem
+            key={task.id}
+            task={task}
+            updateStatus={updateStatus}
+            deleteTask={() => deleteTask(task.id)}
+            editTask={editTask}
+          ></TaskItem>
+        ))}
+      </ul>
+    </section>
+  );
+};
