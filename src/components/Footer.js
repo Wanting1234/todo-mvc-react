9c21023c5bb5f90b824a3a536482b2205386f6e7
Ting Wang <ting.wang@thoughtworks.com>
2022-11-22 14:14:19 +0800

    style: add links in the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,16 @@
 import React from "react";
 
-
 function Footer() {
-    return (
-        <section className="footer">
-            <p>Double-click to edit a todo</p>
-            <p>Created by Ting Wang</p>
-            <p>Part of TodoMvc</p>
-        </section>
-    );
+  return (
+    <section className="footer">
+      <p>Double-click to edit a todo</p>
+      <p>
+        Created by
+        {<a href="https://github.com/Wanting1234/todo-mvc-react"> Ting Wang</a>}
+      </p>
+      <p>Part of {<a href="https://todomvc.com/">TodoMVC</a>}</p>
+    </section>
+  );
 }
 
 export default Footer;
